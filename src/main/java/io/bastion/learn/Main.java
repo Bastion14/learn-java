47d08935e9531abb1a871a2410dcd8b4763e6aaf
Bastion14 <31570937+Bastion14@users.noreply.github.com>
2019-07-31 17:26:28 -0500

    Multi-line Hello program
    Resolves: #1

--- src/main/java/io/bastion/learn/Main.java
+++ src/main/java/io/bastion/learn/Main.java
@@ -2,7 +2,8 @@ package io.bastion.learn;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test"); // Display the string.
+        System.out.println("Hello"); // Display the string.
+        System.out.println("Bastion14");
     }
 
 }
